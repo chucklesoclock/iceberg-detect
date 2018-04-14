094d9890ffec76a5a4d8506872103685ae6cf850
chucklesoclock <charlieg895@gmail.com>
2018-04-13 21:06:31 -0500

    helpers and extraneous code

--- helpers.py
+++ helpers.py
@@ -1,5 +1,6 @@
 import warnings
 import numpy as np
+from functools import reduce
 
 
 def process_df(df):
@@ -7,14 +8,14 @@ def process_df(df):
     if not isinstance(df.band_1.iloc[0], np.ndarray):
         for band in ['band_1', 'band_2']:
             df[band] = df[band].apply(np.asarray)
-            
+
     # set band_3 as mean of first two bands
     if 'band_3' not in df.columns:
         df['band_3'] = (df.band_1 + df.band_2) / 2
         print('Third band added!')
     else:
         print('Third band already present')
-    
+
     with warnings.catch_warnings():
         warnings.simplefilter(action='ignore', category=FutureWarning)
         try:
@@ -25,3 +26,15 @@ def process_df(df):
             print('inc_angle reclassified as floats with NaNs!')
         except TypeError:
             print('inc_angle already all floats')
+
+
+def L(x):
+    return 1 + np.log(x) if x > 1 else x
+
+
+def bentes_norm(arr):
+    """"""
+    Larr = np.fromiter((L(x) for x in arr), dtype=arr.dtype,
+                       count=reduce(lambda a, b: a * b, arr.shape))
+    maxL = abs(Larr.max())
+    return Larr / maxL
--- json_pos.py
+++ json_pos.py
@@ -1,9 +1,31 @@
-import json
-
 print('start'.center(20, '-'))
 with open('predict-is_iceberg.ipynb') as f:
-    # for line in f:
-    #     print(line.rstrip())
-    out_file = ''.join((line.rstrip() for line in f))
+    source = []
+    num_bracket = 0
+    in_source = False
+    for lnum, line in enumerate(f):
+        # print('{:3d}: {}'.format(lnum, line.strip()))
+        lnum += 1
+        sline = line.strip()
+        prefix = '"source": ['
+        if sline.startswith(prefix):
+            num_bracket = 1
+            in_source = True
+            continue
+        elif in_source:
+            if '[' in sline:
+                num_bracket += 1
+            if ']' in sline:
+                num_bracket -= 1
+        if num_bracket == 0:
+            in_source = False
+        else:
+            source.append(sline)
+
+    out_file = '\n'.join(l.strip('"').rstrip('\n",').replace('\\n', '')
+                         for l in source)
+
+with open('predict-is_iceberg-lost_source.txt', 'w') as out:
+    out.write(out_file)
+
 print('end'.center(20, '-'))
-json.loads(out_file)
